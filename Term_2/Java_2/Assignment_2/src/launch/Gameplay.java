60eaeaf39d4971b207f96a0d36e3f38f47adf0f9
Julio Tain Sueiras <juliosueiras@gmail.com>
2014-02-09 19:18:01 -0500

    Added code coverage report for assignment 2

--- Term_2/Java_2/Assignment_2/src/launch/Gameplay.java
+++ Term_2/Java_2/Assignment_2/src/launch/Gameplay.java
@@ -1,6 +1,7 @@
 package launch;
 
 import model.hero.*;
+import model.*;
 /**
  * Created by julio on 2/8/14.
  */
@@ -8,6 +9,6 @@ public class Gameplay
 {
     public void main(String[] args)
     {
-
+        Avatar test = new Superhero(new Superhero.PointSystem(1,2,3,4,5,6,4),1,"","",true,1,"");
     }
 }
