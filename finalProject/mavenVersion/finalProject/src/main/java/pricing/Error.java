e4917963856c3a11570fd3557d4cfd9f026e7362
Julio Tain Sueiras <juliosueiras@gmail.com>
2014-11-19 17:52:42 -0500

    added finalProjectMaven

---
+++ finalProject/mavenVersion/finalProject/src/main/java/pricing/Error.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pricing;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author jhlee
+ */
+public class Error {
+    private int errorCode;
+    private String errorDesc;
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorDesc() {
+        return errorDesc;
+    }
+
+    public void setErrorDesc(String errorDesc) {
+        this.errorDesc = errorDesc;
+    }
+    
+    
+
+}
